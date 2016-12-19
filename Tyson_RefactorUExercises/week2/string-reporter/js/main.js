16a627d875b0c441c81d906bf7d584f70da2f33a
Tyson Gerber <tysongerber@Tysons-MacBook-Pro.local>
2016-12-19 16:13:39 -0700

    finished String Reporter

---
+++ Tyson_RefactorUExercises/week2/string-reporter/js/main.js
@@ -0,0 +1,9 @@
+var answer = prompt('Enter any word')
+alert(answer)
+
+console.log(answer)
+console.log(answer.length)
+console.log(answer.toLowerCase())
+console.log(answer.toUpperCase())
+console.log('This is your word: '+ answer + ' in a sentence')
+console.log(answer.substring(1,4))
