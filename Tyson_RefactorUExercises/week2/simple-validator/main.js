9b54327c6d0c269b45a70ee907c5b3a857f6e283
TysonGerber <tysonrgerber@gmail.com>
2016-12-22 20:10:13 -0700

    end of day notes and finished simple validator exercise

--- Tyson_RefactorUExercises/week2/simple-validator/main.js
+++ Tyson_RefactorUExercises/week2/simple-validator/main.js
@@ -16,17 +16,17 @@ var userInput = prompt('What is your phone number? Use xxx-xxx-xxxx format')
 
 
 if (userInput.charAt(3) === '-' && userInput.charAt(7) === '-' && (userInput.length === 11)) {
-        var phoneCheck = user.Input.split('-')
-    } else if((phoneCheck[0] >= 0) && (phoneCheck[0] <= 9)){
-    
+    var phoneCheck = user.Input.split('-')
+} else if ((phoneCheck[0] >= 0) && (phoneCheck[0] <= 9)) {
+
     // var userString = userInput.toString();
     // alert(+ userString + 'is correct')
-    } else if((phoneCheck[1] >=0) && (phoneCheck[1] <=9)) {
-    
-    }else if ((phoneCheck[2] >=0) && (phoneCheck[2]<=9)){
-        console.log('Your number is: ' + phoneCheck)
-    }else{
-        console.log('try inputing your phone number with dashes and with the area code.')
+} else if ((phoneCheck[1] >= 0) && (phoneCheck[1] <= 9)) {
+
+} else if ((phoneCheck[2] >= 0) && (phoneCheck[2] <= 9)) {
+    console.log('Your number is: ' + phoneCheck)
+} else {
+    console.log('try inputing your phone number with dashes and with the area code.')
 }
 
 
@@ -58,36 +58,62 @@ if (birthDate.charAt(2) === '/' && birthDate.charAt(5) === '/' && birthDate.leng
 // State Abbreviated 2 words
 ////// WHAT I HAD THAT WASN'T WORKING///////////////////////
 var state = prompt('Enter your state abreviated ex: CO');
-    if(state.length === 2){
-        console.log('length correct')
-        for(var i = 0; i < state.length; i++){
-            var stateUpper = state[i].toUpperCase();
+if (state.length === 2) {
+    console.log('length correct')
+    for (var i = 0; i < state.length; i++) {
+        var stateUpper = state[i].toUpperCase();
 
     }
-    console.log('uppercase: ' +stateUpper)
- }
+    console.log('uppercase: ' + stateUpper)
+}
 state('CO')
 
 ////////////////CORRECT WAYS//////////////////////////////
 
 var state = prompt('Enter your state abreviated ex: CO');
 
-    if(state.length === 2){
-        var stateUpper = ''
-        for(var i = 0; i < state.length; i++){
-            stateUpper  += state[i].toUpperCase();
-            
+if (state.length === 2) {
+    var stateUpper = ''
+    for (var i = 0; i < state.length; i++) {
+        stateUpper += state[i].toUpperCase();
+
     }
-  console.log('uppercase: ' +stateUpper)
- }
+    console.log('uppercase: ' + stateUpper)
+}
 
 
 
 
- state = prompt('Enter your state abreviated ex: CO');
+state = prompt('Enter your state abreviated ex: CO');
 
-    if(state.length === 2){
-        var stateUpper = state.toUpperCase()
-       console.log('uppercase: ' +stateUpper)
-          
+if (state.length === 2) {
+    var stateUpper = state.toUpperCase()
+    console.log('uppercase: ' + stateUpper)
+
+}
+
+// var married = prompt('Are you married yes or no?')
+
+// var marriedStatus = function(){
+//     if (married === 'yes' || married === 'YES') {
+//         return married
+//     } else if (married === 'no' || married === 'NO') {
+//         return married
+//     } else{
+//         console.log('please choose yes or no')
+//     }
+// }
+// console.log(marriedStatus())
+
+var married = prompt('Are you married?')
+
+var marriedStatus = married.toUpperCase()
+
+var marriedResults = function(){
+    if (marriedStatus === 'YES' || marriedStatus === 'NO'){
+        return marriedStatus
+    }else {
+        return console.log('Please answer yes or no')
     }
+}
+console.log(marriedResults())
