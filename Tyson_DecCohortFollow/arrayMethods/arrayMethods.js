5314f0c128c575885d3f5863236a4f3762c22d46
TysonGerber <tysonrgerber@gmail.com>
2017-01-24 19:04:17 -0700

    finishing the backend touches to my form on my portfolio page

--- Tyson_DecCohortFollow/arrayMethods/arrayMethods.js
+++ Tyson_DecCohortFollow/arrayMethods/arrayMethods.js
@@ -118,20 +118,137 @@ var func = function (ages) {
 console.log(func(myAges))
 
 
-
-
 //pop()
 
+var myAges = [32, 33, 16, 40]
+
+ var midlife =  myAges.pop()
+console.log(midlife)
 //push()
 
+var retirement = 65;
+
+     myAges.push(retirement)
+
+     console.log(myAges)
+
 //shift()
+
+// same as pop but shifts the 1st element of the array which would be 32
+
 //unshift()
+// same as push add to the front of the array
 
 //reverse() reverses the order of the elements
 
+var reverse = [1,2,3,4,5]
+
+var num = reverse.slice().reverse()
+
+console.log(num)
+
 //slice() takes selects a part of an array that you choose and returns just that section. slice (2,5)
+
+var slicing = ['Tyson', 'Steve', 'Rob', 'Martha']
+
+var newSlice= slicing.slice(1,3)
+
+console.log(newSlice)
+
+//Martha Steve Rob Tyson
+
+var slicing = ['Tyson', 'Steve', 'Rob', 'Martha']
+
+ var slice = slicing.slice() //Martha taken off    Tyson Steve Rob  Pop = Martha
+var Martha = slice.pop()
+var Tyson= slice.shift()
+slice.push(Tyson)
+slice.unshift(Martha)
+
+console.log(slice)
+ 
+  //Tyson taken off Steve Rob Martha Shift = Tyson
+
 
 //sort() give it a function, -1 0 or 1 how you want to sort your array.
 
+var num = [1, 10, 2, 15, 6]
+var alpha = ['b','a','g','d']
+
+var newNum = num.slice()
+
+var numericSort= function(a,b) {
+    if(+a < +b){
+        return -1 
+    }else if (+a == +b){
+        return 0
+    }else if (+a > +b){
+        return +1
+    }
+
+}
+
+// = IS AN ASSIGMENT
+// == IS AN COMPARISON
+
+alpha.sort() //alphabetically by default
+console.log(num.sort(numericSort))  //1,10,15,2,6
+console.log(alpha) //a,b,d,g
+
+
+
+// EXAMPLE #2 SORT()
+
+
+var num = [1, 10, 2, 15, 6]
+var alpha = ['b','a','g','d']
+
+var newNum = num.slice()
+
+var numericSort2= function(a,b) {
+
+    return a-b 
+}
+            // 1-10 = -9 // make the 1 before the 10 
+            // 1-1 = 0   // don't change them
+            // 10-1 = -9 // make the 10 before the 1
+            // if(+a < +b){
+            //     return -1 
+            // }else if (+a == +b){
+            //     return 0
+            // }else if (+a > +b){
+            //     return +1
+            // }
+
+
+    // 
+console.log(num.sort(numericSort2))
+
 //splice() (rarely used) you tell it where to insert into the array. fruits.splice(2, 0 , "lemon", kiwi)
 
+var myFish = ['angel', 'clown', 'mandarin', 'trout' ]
+
+myFish.splice(3,0,'goldfish')
+
+console.log(myFish) //[ 'angel', 'clown', 'mandarin', 'goldfish', 'trout' ]
+
+myFish.splice(2,1)
+
+console.log(myFish) //[ 'angel', 'clown', 'goldfish', 'trout' ]
+
+
+
+
+//EXAMPLE 2
+var myFish = ['angel', 'clown', 'mandarin', 'trout' ]
+
+
+   var angel = myFish.splice(0,1) //angel off
+    console.log(myFish) //angel
+    myFish.splice(3,0,angel[0]) //angel on the end
+    console.log(myFish)
+   var trout = myFish.splice(2,1) //takes trout off
+    console.log(myFish)
+    myFish.splice(0,0,trout[0])// put trout at front
+    console.log(myFish)
+    
--- Tyson_portfolio/public/js/controller.js
+++ Tyson_portfolio/public/js/controller.js
@@ -1,9 +1,20 @@
 angular.module('portfolio', [])
     .controller('pController', pFunction)
 
-    function pFunction (){
+pFunction.$inject = ['$http'];
+
+    function pFunction ($http){
         var pCtrl = this;
         pCtrl.title = 'Welcome to my portfolio page'
+        pCtrl.user = {};
 
-
+        pCtrl.createUser = ()=>{
+            console.log('trying to create user')
+            $http.post('/api/user', pCtrl.user).then((success)=>{
+                console.log('success adding user to db:', success.data);
+                var userData = success.data;
+            }, (error)=>{
+                console.log('Error saving user to db', error);
+            })
+        }
     };
