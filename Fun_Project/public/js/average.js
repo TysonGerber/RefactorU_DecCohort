452cdacfcdf5c9248f38d4107b46073b1f7dced1
TysonGerber <tysonrgerber@gmail.com>
2017-02-15 16:47:19 -0700

    added headings and made function for each button versus directly inputting days into the angular funcitons

--- Fun_Project/public/js/average.js
+++ Fun_Project/public/js/average.js
@@ -10,67 +10,67 @@ function stock($http, $event) {
 
     stock.greeting = 'Welcome to your stock page'
 
-    //Buttons 1D, 1W, 1M, 6M, 1YR, 5Yr, Max.
-    //1D
-    //     stock.oneDay = function($event){
-    //         console.log('1 day', 1);
-    //         return 1;
-    //     }
-    //     //1 Week
-    //      stock.oneWeek = function($event){
-    //         console.log('1 week', 7);
-    //         return 7;
-    //     }
-    //     //1 month
-    //      stock.oneMonth = function($event){
-    //         console.log('1 month', 30);
-    //         return 30;
-    //     }
-    //     //6 Months
-    //      stock.sixMonths = function($event){
-    //         console.log('6 months', 180);
-    //         return 180;
-    //     }
-    //     //1 Year
-    //       stock.oneYear = function($event){
-    //         console.log('1 Year', 365);
-    //         return 365;
-    //     }
-    //     //5 Years
-    //      stock.fiveYears = function($event){
-    //         console.log('5 years', 1825);
-    //         return 1825;
-    //     }
-    //     //Max ?? Don't know how to get this one.
-    //     stock.Max = function($event){
-    //         console.log('6 months', 180);
-    //         return 'max';
-    //     }
+   
+var timePeriod = {
+    
+    text: ' Today\'s date',
+    number: "",
+    days: 1
+    
+}
 
-    // function convertMS(ms) {
-    //   var d, h, m, s;
-    //   s = Math.floor(ms / 1000);
-    //   m = Math.floor(s / 60);
-    //   s = s % 60;
-    //   h = Math.floor(m / 60);
-    //   m = m % 60;
-    //   d = Math.floor(h / 24);
-    //   h = h % 24;
-    //   console.log({ d: d, h: h, m: m, s: s })
-    //   return { d: d, h: h, m: m, s: s };
-    // };
+        stock.oneDay = function(){
+            timePeriod.text = " Today's date"
+            timePeriod.days= 1
+            console.log('One day being CLICKED')
+        }
+      
+        //1 Week
+         stock.oneWeek = function(){
+               timePeriod.text = " past week"
+               timePeriod.days = 7;
+            console.log('1 week', 7);
+        }
+        
+        //1 month
+         stock.oneMonth = function(){
+              timePeriod.text = " current month"
+               timePeriod.days = 30;
+            console.log('1 month', 30);
+         
+        }
+        //6 Months
+         stock.sixMonths = function(){
+              timePeriod.text = " the last 6 months"
+               timePeriod.days = 180;
+            console.log('6 months', 180);
+    
+        }
+        //1 Year
+          stock.oneYear = function(){
+               timePeriod.text = " last year"
+               timePeriod.days = 365;
+            console.log('1 Year', 365);
+          
+        }
+        //5 Years
+         stock.fiveYears = function(){
+              timePeriod.text = " the last five years"
+               timePeriod.days = 1825;
+            console.log('5 years', 1825);
+         
+        }
+        //Max ?? Don't know how to get this one.
+        stock.Max = function(){
+             timePeriod.text 
+               timePeriod.days
+            console.log('6 months', 180);
+           
+        }
 
 
 
-
-    // stock.week = moment() - moment().subtract(7,'week')
-    // stock.month = moment() - moment().subtract(30,'month')
-    // stock.halfyear = moment() - moment().subtract(180,'six months')
-    // stock.year = moment() - moment().subtract(365,'year')
-    // stock.five = moment() - moment().subtract(1825,'five years')
-
-
-    // AVERAGE STOCK PRICE BAR CHART (FROM HIGHCHARTS)
+    // AVERAGE PRICE STOCK CHART(FROM HIGHCHARTS)
     stock.avgChart = function () {
         var chart = new Highcharts.Chart({
             chart: {
@@ -81,7 +81,7 @@ function stock($http, $event) {
                 text: 'Average Stock Price',
             },
             subtitle: {
-                text: 'Source dev.markitondemand.com'
+                // text: 'Source dev.markitondemand.com'
             },
             xAxis: {
                 categories: [stock.company],
@@ -119,9 +119,9 @@ function stock($http, $event) {
         });
 
     }
-    // stock.avgChart();
 
-    // Data gathered from http://populationpyramid.net/germany/2015/
+
+    // AVERAGE PRICE STOCK CHART(FROM HIGHCHARTS)
 
     // Age categories
     var categories = ['0-4', '5-9', '10-14', '15-19',
@@ -130,20 +130,20 @@ function stock($http, $event) {
         '70-74', '75-79', '80-84', '85-89', '90-94',
         '95-99', '100 + '];
 
+
     stock.buySellChart = function () {
         console.log('chart stock.buy', stock.buy)
         var chart = new Highcharts.Chart({
             chart: {
                 renderTo: 'buySellContainer',
-                renderTo: 'buySellContainer',
                 type: 'bar'
             },
-            colors: ['rgb(000, 000, 000)', 'rgb(200, 200, 255)'],
+            // colors: ['rgb(000, 000, 000)', 'rgb(200, 200, 255)'],
             title: {
-                text: 'Population pyramid for Germany, 2015'
+                text: 'Buying or Selling power'
             },
             subtitle: {
-                text: 'Source: <a href="http://populationpyramid.net/germany/2015/">Population Pyramids of the World from 1950 to 2100</a>'
+                text: "based on average stock price and " + timePeriod.number + timePeriod.text +""
             },
             xAxis: [{
                 categories: categories,
@@ -191,15 +191,12 @@ function stock($http, $event) {
                 name: 'Sell',
                 data: stock.sell
             },
-          ]
+            ]
         });
     }
-    // stock.buySellChart();
 
 
-    // Function for finding the average of stock
-
-    //Grabbing the array of prices and FINDING THE AVERAGE PRICE
+    // FUNCTION FOR FINDING THE AVERAGE PRICE (grabbing the array of prices from the api (stockReturn.Elements[0].DataSeries.close.values) and FINDING THE AVERAGE PRICE)
     function addThemAll(numbers) {
         var sum = 0
         for (var i = 0; i < numbers.length; i++) {
@@ -210,40 +207,37 @@ function stock($http, $event) {
         return average
     }
 
+
     stockInfo = [];
-
     stock.getSymbol = {
-        submit: function (days) {
-            stock.days = days
-            // // if(days === 0){
-            // // stock.average == stock.YaxisLP
-            // //console.log(stock.average)
-            // if (days === 'null' || days === [0]) {
-            //     days === stock.YaxisLP
-            // }
+        submit: function () {
+            stock.days = timePeriod.days
+         
+            
+            //1st PROMISE
+            var stockPromise = $http.get('/stock' + '?symbol=' + stock.symbol);
 
 
-         var stockPromise =   $http.get('/stock' + '?symbol=' + stock.symbol);
-       
-            // passing in as an argument on submit button 1D, 1W, 1M etc
-
-
-          var chartPromise =  $http.get('/chart?symbol=' + stock.symbol + '&days=' + stock.days);
-          Promise.all([stockPromise, chartPromise]).then(function success(resArray)    {
+            // 2nd PROMISE
+            var chartPromise = $http.get('/chart?symbol=' + stock.symbol + '&days=' + stock.days);
+            Promise.all([stockPromise, chartPromise]).then(function success(resArray) {
                 // var chart = JSON.parse(res.data)
                 // console.log('Chart data successful', res.data)
                 // stock.chart = chart
 
                 //stock Promise data
+                console.log(resArray[0].data)
                 var info = JSON.parse(resArray[0].data)
+                
                 // console.log('stock?symbol success', info.LastPrice)
                 stock.info = info
                 // console.log('stock.info', stock.info)
                 stock.company = stock.info.Name
                 // console.log('stock.company', stock.company)
-                stock.YaxisLP = stock.info.LastPrice
-                // console.log('stockYaxisLP', stock.YaxisLP)
-                // stock.avgChart();
+                stock.YaxisLP = Number(Math.round(stock.info.LastPrice+'e1')+'e-1')  //rounding to the 2nd decimal example 1.05 = 1.1
+                console.log('stockYaxisLP', stock.YaxisLP)
+                
+
 
 
                 //chart Promise data
@@ -253,42 +247,52 @@ function stock($http, $event) {
                 // console.dir(stockReturn)
                 // console.log('stockReturn', stockReturn)
 
-                // Xaxis is time period the graph is based off of.
-                stock.Xaxis = 'Today\'s date'
 
-                   // Getting the average for stock
+
+                // Getting the average for stock
                 var numArray = stockReturn.Elements[0].DataSeries.close.values
                 var average = addThemAll(numArray)
-                stock.average = average
-                // console.log('stock.average', stock.average);
-                
-             
+                stock.average =  Number(Math.round(average +'e1')+'e-1')  //rounding to the 2nd decimal example 1.05 = 1.1
+                console.log('stock.average', stock.average);
+
+
+
+
+
 
                 // Getting the percentage of stock based on the average price and today's stock price.
                 stock.buy = [];
                 stock.sell = [];
                 var diffInPrice = (+stock.average - +stock.YaxisLP);
                 stock.percentage = (+diffInPrice / +stock.YaxisLP * 100);
-                console.log('figure it out stock.percentage', typeof stock.percentage)
-                if(stock.percentage > 0){
-                    //If it is a positive number then SELL // stock.sell.push.
-                    stock.sell.push(+stock.percentage) 
-                      console.log('This is stock.sell, number should be POSITIVE pushing THIS ONE', stock.sell)  
-                    //Created this one because I need the opposite side of the graph to appear. I then am making this one transparent.
-                    stock.sell.push(-stock.percentage) 
+                // console.log('figure it out stock.percentage', typeof stock.percentage)
 
-                    //ASK HOW TO GRAB THE ELEMENT BY CLASS NAME AND WHY THIS ISN'T WORKING.
-                    // document.getElementsByClassName("#highcharts-r2qss7l-8 > svg > g.highcharts-series-group > g.highcharts-series.highcharts-series-1.highcharts-bar-series.highcharts-color-1.highcharts-tracker > rect.highcharts-point.highcharts-negative.highcharts-color-1").style.color='purple'
-                    console.log('PURPLE stock.buy OPPOSITE', stock.sell)  
-                                   
-                }else {
+
+
+                if (+stock.percentage > 0.9 && +stock.percentage < -0.9) {
+                    //If it is a positive number then SELL // stock.sell.push.
+                    stock.sell.push(+stock.percentage)
+                    console.log('This is stock.sell, number should be POSITIVE pushing THIS ONE', stock.sell)
+
+                    //Created this one because I need the opposite side of the graph to appear. I then am making this one transparent.
+                    stock.sell.push(-stock.percentage)
+
+                    console.log('PURPLE stock.buy OPPOSITE', stock.sell)
+
+                }
+                 //else if (Math.floor(Math.abs(stock.percentage)) == 0) {
+               //     console.log('stock.percentage is 0', stock.percentage)
+               // } 
+                else {
                     //If it is a negative number then BUY // stock.buy.push.
                     stock.buy.push(+stock.percentage)
                     console.log('This is stock.buy, number should be NEGATIVE pushing THIS ONE', stock.buy)
-                       stock.buy.push(-stock.percentage) 
+                    stock.buy.push(-stock.percentage)
                     console.log('stock.sell OPPOSITE', stock.buy)
-                      
+
                 }
+
+
                 // console.log('stock.buy', stock.buy)
                 // console.log('stock.sell', stock.sell)
 
@@ -297,11 +301,8 @@ function stock($http, $event) {
                 // console.log('diff in price', diffInPrice);
                 // console.log('percentage', stock.percentage);
 
-
                 stock.avgChart();
                 stock.buySellChart();
-
-
             },
                 function failed(res) {
                     console.log('')
@@ -312,65 +313,13 @@ function stock($http, $event) {
         }
     }
 
-}
+};
 
 
 
 
 
 
-// Or you can do this: 
-//      stock.getTicker = function(event) {
 
-//         $http.get('http://dev.markitondemand.com/Api/v2/Quote/jsonp/?symbol='+ stock.ticker)
-//         .then(function(res){
-//             stock.quote = res.data
-//             console.log(res.data)
-//             console.log(event)
-//         },
-//         function(err){
-//             console.log('Error loading Market API', err)
-//         });
-
-//     }
-
-
-
-    //     stock.avgChart = function () {
-    //         var chart = new Highcharts.Chart({
-    //             chart: {
-    //                 renderTo: 'container',
-    //                 alignTicks: false
-    //             },
-
-    //             rangeSelector: {
-    //                 selected: 1
-    //             },
-
-    //             title: {
-    //                 text: 'AAPL Stock Volume'
-    //             },
-
-    //             series: [{
-    //                 type: 'column',
-    //                 name: stock.Yaxis,
-    //                 data:  [stock.Yaxis],
-    //                 dataGrouping: {
-    //                     units: [[
-    //                         // 'week', // unit name
-    //                         // [1] // allowed multiples
-    //                     ], [
-    //                         // 'month',
-    //                         // [1, 2, 3, 4, 6]
-    //                     ]]
-    //                 }
-
-    //             }]
-
-    //         });
-    // console.log('second', stock.Yaxis)
-    //     }
-
-    //     stock.avgChart();
 
 
--- Fun_Project/public/js/testarea.js
+++ Fun_Project/public/js/testarea.js
@@ -37,24 +37,28 @@
 // doStuff();
 
 
+
+
+
+
 // Testing how to push days into an array and display when you push 1D, 1W, etc buttons.
-days =[];
+// days =[];
 
-  function oneDay(time){
-      days.push(time);
-    }
+//   function oneDay(time){
+//       days.push(time);
+//     }
    
-oneDay(1)
-console.log(days)
+// oneDay(1)
+// console.log(days)
 
-stock.days = [];
+// stock.days = [];
 
-   stock.oneDay = function(time){
-       stock.days.push(time)
+//    stock.oneDay = function(time){
+//        stock.days.push(time)
         
-    }
- stock.oneDay(1)
- console.log(stock.days)
+//     }
+//  stock.oneDay(1)
+//  console.log(stock.days)
 
 
 
@@ -62,7 +66,144 @@ stock.days = [];
 
 
 
-var numbers = [];
-numbers.push(1);
+// var numbers = [];
+// numbers.push(1);
 
-console.log(numbers); // [1, 2, 3, 4]
+// console.log(numbers); // [1, 2, 3, 4]
+
+
+
+
+
+
+//I WAS GOING TO USE MOMENT.JS and learned this was easier for my project.
+
+    //Buttons 1D, 1W, 1M, 6M, 1YR, 5Yr, Max.
+    //1D
+        // stock.oneDay = function($event){
+        //     console.log('1 day', 1);
+        //     return 1;
+        // }
+        // //1 Week
+        //  stock.oneWeek = function($event){
+        //     console.log('1 week', 7);
+        //     return 7;
+        // }
+        // //1 month
+        //  stock.oneMonth = function($event){
+        //     console.log('1 month', 30);
+        //     return 30;
+        // }
+        // //6 Months
+        //  stock.sixMonths = function($event){
+        //     console.log('6 months', 180);
+        //     return 180;
+        // }
+        // //1 Year
+        //   stock.oneYear = function($event){
+        //     console.log('1 Year', 365);
+        //     return 365;
+        // }
+        // //5 Years
+        //  stock.fiveYears = function($event){
+        //     console.log('5 years', 1825);
+        //     return 1825;
+        // }
+        // //Max ?? Don't know how to get this one.
+        // stock.Max = function($event){
+        //     console.log('6 months', 180);
+        //     return 'max';
+        // }
+
+    // function convertMS(ms) {
+    //   var d, h, m, s;
+    //   s = Math.floor(ms / 1000);
+    //   m = Math.floor(s / 60);
+    //   s = s % 60;
+    //   h = Math.floor(m / 60);
+    //   m = m % 60;
+    //   d = Math.floor(h / 24);
+    //   h = h % 24;
+    //   console.log({ d: d, h: h, m: m, s: s })
+    //   return { d: d, h: h, m: m, s: s };
+    // };
+
+
+
+
+    // stock.week = moment() - moment().subtract(7,'week')
+    // stock.month = moment() - moment().subtract(30,'month')
+    // stock.halfyear = moment() - moment().subtract(180,'six months')
+    // stock.year = moment() - moment().subtract(365,'year')
+    // stock.five = moment() - moment().subtract(1825,'five years')
+
+
+
+
+
+    // A different chart I was looking at using.
+
+    //     stock.avgChart = function () {
+    //         var chart = new Highcharts.Chart({
+    //             chart: {
+    //                 renderTo: 'container',
+    //                 alignTicks: false
+    //             },
+
+    //             rangeSelector: {
+    //                 selected: 1
+    //             },
+
+    //             title: {
+    //                 text: 'AAPL Stock Volume'
+    //             },
+
+    //             series: [{
+    //                 type: 'column',
+    //                 name: stock.Yaxis,
+    //                 data:  [stock.Yaxis],
+    //                 dataGrouping: {
+    //                     units: [[
+    //                         // 'week', // unit name
+    //                         // [1] // allowed multiples
+    //                     ], [
+    //                         // 'month',
+    //                         // [1, 2, 3, 4, 6]
+    //                     ]]
+    //                 }
+
+    //             }]
+
+    //         });
+    // console.log('second', stock.Yaxis)
+    //     }
+
+    //     stock.avgChart();
+
+
+
+
+// another way to do the same thing 
+
+
+// Or you can do this: 
+//      stock.getTicker = function(event) {
+
+//         $http.get('http://dev.markitondemand.com/Api/v2/Quote/jsonp/?symbol='+ stock.ticker)
+//         .then(function(res){
+//             stock.quote = res.data
+//             console.log(res.data)
+//             console.log(event)
+//         },
+//         function(err){
+//             console.log('Error loading Market API', err)
+//         });
+
+//     }
+
+var x = Math.floor(Math.abs(-0.6))
+
+console.log(x)
+
+var y = Number(Math.round(-1.07+'e1')+'e-1')
+console.log(y)
